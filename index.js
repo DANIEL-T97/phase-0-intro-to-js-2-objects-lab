118c4e7b6977de0dbfa98f8f073502b0ee91aca0
Daniel <daniel.mwangi@student.moringaschool.com>
2023-09-23 21:03:44 +0300

    committed js objects

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    Name: "Daniel Mwangi",
+    streetAddress:"467 Eldama Ravine",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee= { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
